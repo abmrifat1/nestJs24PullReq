42c5988a4579d716050436d49f6be93f58f64ef3
abmrifat1 <rifatabm1@gmail.com>
2022-10-07 12:31:25 +0600

    create a api for test and thats working!

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,4 +1,9 @@
 import { Module } from "@nestjs/common";
+import { UserController } from "./user.controller";
+import { UserService } from "./user.service";
 
-@Module({}) // decorator
+@Module({ // decorator
+    controllers: [UserController],
+    providers: [UserService]
+}) 
 export class UserModule{}
